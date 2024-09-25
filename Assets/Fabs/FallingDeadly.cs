e3169df9fceaa2ae9d697afb1d4c34523643e24c
azern32 <chiro.emiya@gmail.com>
2024-09-25 14:07:10 +0800

    bakal jadi rage game ini

--- Assets/Fabs/FallingDeadly.cs
+++ Assets/Fabs/FallingDeadly.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class FallingDeadly : MonoBehaviour
 {
-    public float fallingspeed = 2;
+    public float fallingspeed;
     public float deadzone = -140;
     // Start is called before the first frame update
     void Start()
     {
-
+        fallingspeed = Random.Range(40, 60);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, -fallingspeed, 0);
+        transform.position += new Vector3(0, -fallingspeed * Time.deltaTime, 0);
 
         if (transform.position.y < deadzone)
         {
+            Debug.Log("ID: " + gameObject.GetInstanceID() + ", Current falling speed: " + fallingspeed);
             Destroy(gameObject);
         }
     }
